b50cf6228b3b42fc0aaeb3b093d597360f038a1c
Santiago Ovejero <santiagodovejero@gmail.com>
2021-10-18 19:44:28 -0300

    feat: add skeleton

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        GOODWOD
+      </div>
+      
+      <div>
+      Contactanos
+      +549 2215343580
+      disenogoodwood@gmail.com
+      Showroom
+      Calle 23 nro 1574 1/2 Taller Dinamita
+      La Plata, Bs.As. Arg.
+      </div>
+
+      <div>
+      Redes Sociales
+      </div>
+
+    </div>
+  );
+};
+
+export default Footer;
