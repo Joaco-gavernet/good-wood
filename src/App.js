cf9e9c0ffd168a5ce9a27b0a4c241b11033f9e9d
Santiago Ovejero <santiagodovejero@gmail.com>
2021-10-24 21:13:11 -0300

    update file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
+import Header from './components/Header/Header.js';
+import Banner from './components/Banner/Banner.js';
+import About from './components/About/About.js';
+import Hero from './components/Hero/Hero.js';
+import Whatsapp from './components/Whatsapp/Whatsapp.js';
+import Questions from './components/Questions/Questions.js';
+import Products from './components/Products/Products.js';
+import Contact from './components/Contact/Contact.js';
+import Footer from './components/Footer/Footer.js';
 
-// Components 
-import Header from './components/Header/Header';
-import About from './components/About/About';
-import Hero from './components/Hero/Hero';
-import Nav from './components/Nav/Nav';
-import Whatsapp from './components/Whatsapp/Whatsapp';
-import Questions from './components/Questions/Questions';
-import Products from './components/Products/Products';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-
-// Styles
 import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <About />
+      <Banner />
+{/*   <About />
       <Hero />
-      <Nav />
       <Whatsapp />
       <Questions />
       <Products />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
