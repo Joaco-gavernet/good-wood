698ae8726b471838b407151257356500e197b46d
Santiago Ovejero <santiagodovejero@gmail.com>
2021-10-26 17:54:16 -0300

    feat: add new compnents

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header.js';
 import Banner from './components/Banner/Banner.js';
 import About from './components/About/About.js';
 import Hero from './components/Hero/Hero.js';
+import Information from './components/Information/Information.js';
 import Whatsapp from './components/Whatsapp/Whatsapp.js';
 import Questions from './components/Questions/Questions.js';
 import Products from './components/Products/Products.js';
@@ -16,8 +17,10 @@ const App = () => {
     <div className="App">
       <Header />
       <Banner />
-{/*   <About />
       <Hero />
+      <Information />
+{/*   <About />
+ 
       <Whatsapp />
       <Questions />
       <Products />
