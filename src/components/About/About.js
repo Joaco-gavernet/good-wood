f9bff1b553b01ba41b5e5c4ca63df0f3d9bdec4c
Joaquin Gavernet <joaquingavernet@gmail.com>
2021-10-28 21:04:10 -0300

    About finished.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,9 @@ import "./About.scss";
 
 const About = () => {
   return (
-    <div>
+    <div className="about">
+      <h2 className="about__title">Sobre nosotros</h2>
+      <p className="about__text">Goodwood, surge a partir de la reutilización y resignificación de materiales de descarte, principalmente la madera. <br /><br /> Creamos juguetes de madera para niños de entre 2 y 10 años, a través de la utilización de materiales nobles y formas puras para estimular los sentidos, la creatividad y la imaginación.</p>
     </div>
   );
 };
