e863581bf26cd17e23954e2a8ec733e8210edf5f
Santiago Ovejero <santiagodovejero@gmail.com>
2021-11-14 12:40:21 -0300

    feat: add contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,10 @@ import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
+      <p className="contact__text">Juguetes responsables con el medio
+      ambiente, de formas reconocibles, fáciles de manipular y trasladar.</p>
+      <a className="contact__button">¡Contactanos!</a>
     </div>
   );
 };
