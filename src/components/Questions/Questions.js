9cef11245b43274af8413fcbd53bcdcac5d6f891
Joaquin Gavernet <joaquingavernet@gmail.com>
2021-11-20 20:26:01 -0300

    Nav functionality. Back to Top implementation.

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -5,30 +5,30 @@ import "./Questions.scss";
 
 const Questions = () => {
   return (
-    <div>
+    <div className="questions" id='questions'>
       <div className="question">
         <p className="question__title">Preguntas frecuentes</p>
         <div className="question__text">
           <div className="question__text--block">
             <p className="question__paragraph"><strong>¿Para que edad se recomienda Goodwood?</strong><br></br>
-            Nuestros juguetes están diseñados para niños de entre 2 y 10 años.</p>
-            
+              Nuestros juguetes están diseñados para niños de entre 2 y 10 años.</p>
+
             <p className="question__paragraph"><strong>¿Cuál es el costo de envío?</strong><br></br>
-            El costo de envío varía según el total de la compra y tu ubicación.</p>
+              El costo de envío varía según el total de la compra y tu ubicación.</p>
 
             <p className="question__paragraph"><strong>¿Dónde puedo recibir mi pedido?</strong><br></br>
-            Realizamos envíos a todo el país.</p>
+              Realizamos envíos a todo el país.</p>
           </div>
 
           <div className="question__text--block">
             <p className="question__paragraph"><strong>¿Para que edad se recomienda Goodwood?</strong><br></br>
-            Nuestros juguetes están diseñados para niños de entre 2 y 10 años.</p>
+              Nuestros juguetes están diseñados para niños de entre 2 y 10 años.</p>
 
             <p className="question__paragraph"><strong>¿Cuál es el costo de envío?</strong><br></br>
-            El costo de envío varía según el total de la compra y tu ubicación.</p>
+              El costo de envío varía según el total de la compra y tu ubicación.</p>
 
             <p className="question__paragraph"><strong>¿Dónde puedo recibir mi pedido?</strong><br></br>
-            Realizamos envíos a todo el país.</p>
+              Realizamos envíos a todo el país.</p>
           </div>
         </div>
       </div>
